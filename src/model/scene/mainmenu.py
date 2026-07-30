28e62b8d75a179bab9d4fe858f3341b9d0c9f78f
Naoaki Sato <nsato@c2r5s10.42tokyo.jp>
2026-07-30 17:31:54 +0900

    feat: スペースキーでplay画面へ遷移。
    タイトル画面リセット未対応。

--- src/model/scene/mainmenu.py
+++ src/model/scene/mainmenu.py
@@ -1,3 +1,8 @@
+"""
+Todo:
+    - [ ] SCREEN_WIDTH, SCREEN_HEIGHTは共通のはずなので、そこからimportしたい。
+    - [ ] 文字の大きさ等要調整。
+"""
 import pygame
 from typing import Any
 
@@ -10,16 +15,32 @@ SCREEN_HEIGHT = 600
 
 class MainMenu(Scene):
 
-    FONT_SIZE = 128
+    TITLE_SIZE = 128
+    INFO_SIZE = 48
     BACKGROUND_COLOR = (0, 0, 0)  # 黒
     TITLE_COLOR = (255, 255, 0)   # 黄色
-    MENU_COLOR = (255, 255, 255)   # 白
+    INFO_COLOR = (255, 255, 255)   # 白
 
     def __init__(self, config: ConfigModel):
         super().__init__(config)
 
         # 初期フォントの設定(これがないと文字がかけないので必要)
-        self.font = pygame.font.Font(None, self.FONT_SIZE)
+        self.font: dict[str, pygame.font.Font] = {
+            "title": pygame.font.Font(None, self.TITLE_SIZE),
+            "info": pygame.font.Font(None, self.INFO_SIZE)
+        }
+
+    def update(self, events: list[pygame.event.Event]) -> None | tuple[str, Any]:
+        """
+        イベントを処理する。
+        画面遷移が必要な場合はシーン名と受け渡すデータをタプルで返す。
+        何もなければNoneを返す
+        """
+        # ===== "PLAY"から"MAIN_MENU"に変更してます。 =====
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                return ("PLAY", None)
+        return ("MAIN_MENU", None)
 
     def _string_put(
         self,
@@ -27,34 +48,70 @@ class MainMenu(Scene):
         x: int,
         y: int,
         color: tuple[int, int, int],
-        string: str
+        string: str,
+        type: str = "info"
     ) -> None:
         # font.render(text, antialias, color, background=None) -> Surface:
         # antialiasはTrueで文字が滑らかになるが、処理が重くなる。今回は✕！
-        text_surface = self.font.render(string, True, color)
+        text_surface = self.font[type].render(string, False, color)
         # surface.blit(source, dest, area=None, special_flags=0) -> Rect
         # 今回はsource=描画する元のサーフェス、dest=(x, y)
         screen.blit(text_surface, (x, y))
 
-    def update(self, events: list[pygame.event.Event]) -> None | tuple[str, Any]:
-        """
-        イベントを処理する。画面遷移が必要な場合はシーン名と受け渡すデータをタプルで返す。
-        何もなければNoneを返す
-        """
-        # ===== "PLAY"から"MAIN_MENU"に変更してます。 =====
-        return ("MAIN_MENU", None)
+    def _draw_title(self, screen: pygame.Surface) -> None:
+        """タイトルの描画
 
-    def draw(self, screen: pygame.Surface) -> None:
-        # 指定した色で画面を塗りつぶす
-        screen.fill(self.BACKGROUND_COLOR)
-
+        Todo:
+            - [ ] 中の// 2の部分なに？調整で入れたけどわからん
+        """
         # タイトルの描画
         title_text = "PAC-MAN"
         # 画面全体の横の長さ - 文字の長さ // 2で横方向中央に配置
-        title_x = (SCREEN_WIDTH - len(title_text) * self.FONT_SIZE // 2) // 2
+        title_x = (SCREEN_WIDTH - len(title_text) * self.TITLE_SIZE // 2) // 2
         # 画面全体の高さ // 12で縦方向に配置
-        title_y = SCREEN_HEIGHT // 12
+        title_y = SCREEN_HEIGHT // 20
+        self._string_put(
+            screen, title_x, title_y, self.TITLE_COLOR, title_text, "title"
+        )
+
+    def _draw_info(self, screen: pygame.Surface) -> None:
+        """タイトル下、サブタイトルの描画
+
+        Todo:
+            - [ ] 点滅可能ならさせたい。
+            - [ ] // 2.5の部分は文字の大きさに応じて調整する(なんでやねん)
+        """
+        info_text = "Push SPACE to play"
+        info_x = (SCREEN_WIDTH - len(info_text) * self.INFO_SIZE // 2.5) // 2
+        info_y = SCREEN_HEIGHT // 3
+        self._string_put(
+            screen, info_x, info_y, self.INFO_COLOR, info_text
+        )
+
+    def _draw_score(self, screen: pygame.Surface) -> None:
+        """スコアの描画
+
+        Todo:
+            - [ ] スコアを取得してハイスコア上位n件をループで描画するようにする。
+            - [ ] // 3の部分は文字の大きさに応じて調整する(ほんとになんで)
+        """
+        score_text = "This space is for score."
+        score_x = (SCREEN_WIDTH - len(score_text) * self.INFO_SIZE // 3) // 2
+        score_y = SCREEN_HEIGHT // 2
 
         self._string_put(
-            screen, title_x, title_y, self.TITLE_COLOR, title_text
+            screen, score_x, score_y, self.INFO_COLOR, score_text
         )
+
+    def draw(self, screen: pygame.Surface) -> None:
+        """タイトル画面の描画。
+
+        Todo:
+            - [ ] spaceを押すとゲームが始まるようにする。
+        """
+        # 指定した色で画面を塗りつぶす
+        screen.fill(self.BACKGROUND_COLOR)
+
+        self._draw_title(screen)
+        self._draw_info(screen)
+        self._draw_score(screen)
