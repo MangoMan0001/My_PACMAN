2d6a1400a54131729d19fe3d23f3b3e10b0b3423
Naoaki Sato <nsato@c5r3s8.42tokyo.jp>
2026-08-04 23:21:35 +0900

    feat: is_wall()を追加。

--- src/model/map.py
+++ src/model/map.py
@@ -178,6 +178,32 @@ class Map(Entity):
                 return not bool(cell & 8)
         return False
 
+    def is_wall(self, x: int, y: int, direction: Direction) -> bool:
+        """指定されたセル座標に壁があるか判定する。
+
+        wall_mapの各要素は16進数で各方向の壁の有無を表す。
+        壁なら1: 上、2: 右、4: 下、8: 左のビットが立っている。
+
+        Args:
+            x (int): セル単位でのx座標
+            y (int): セル単位でのy座標
+            direction (Direction): 判定する方向を表すDirection列挙型
+
+        Returns:
+            bool: 壁がない(通れる)場合はTrue、壁がある場合はFalse
+        """
+        cell = self.wall_map[y][x]
+        if direction == Direction.UP:
+            return not bool(cell & 1)
+        elif direction == Direction.RIGHT:
+            return not bool(cell & 2)
+        elif direction == Direction.DOWN:
+            return not bool(cell & 4)
+        elif direction == Direction.LEFT:
+            return not bool(cell & 8)
+
+        return False
+
     def is_center(self, px: int, py: int) -> tuple[int, int] | None:
         """指定されたピクセル座標が位置するセル内で、中心にいるか判定する。
         中心にいる場合はそのセルの座標を返し、中心にいない場合はNoneを返す。
