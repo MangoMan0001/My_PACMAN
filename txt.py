b4678e32d900d7d90e00fe42805565206dce7f65
Naoaki Sato <nsato@c5r3s8.42tokyo.jp>
2026-08-04 20:14:54 +0900

    chore: 画像生成のためにコードを追加。

--- txt.py
+++ txt.py
@@ -51,7 +51,7 @@ def create_font_lower_images(output_dir: str) -> None:
 def create_font_misaki_images(output_dir: str) -> None:
     pygame.font.init()
     # フォントを使用。サイズ64
-    font = pygame.font.Font("data/font/misaki/misaki_gothic_2nd.ttf", 64)
+    font = pygame.font.Font("data/font/misaki/misaki_gothic_2nd.ttf", 24)
 
     # A〜Zまでの文字画像をループで一気に生成
     for char_code in range(ord('A'), ord('Z') + 1):
@@ -83,14 +83,48 @@ def create_font_misaki_images(output_dir: str) -> None:
     pygame.image.save(text_surface, os.path.join(output_dir, "misaki-FONT_..png"))
 
 
+def create_font_none_images(output_dir: str) -> None:
+    pygame.font.init()
+    # フォントを使用。サイズ64
+    font = pygame.font.Font(None, 32)
+
+    # A〜Zまでの文字画像をループで一気に生成
+    for char_code in range(ord('A'), ord('Z') + 1):
+        char = chr(char_code)
+        # 文字を画像化（黄色）
+        text_surface = font.render(char, True, (255, 255, 255))
+        pygame.image.save(text_surface, os.path.join(output_dir, f"none-FONT_{char}.png"))
+
+    # a〜zまでの文字画像をループで一気に生成
+    for char_code in range(ord('a'), ord('z') + 1):
+        char = chr(char_code)
+        # 文字を画像化（黄色）
+        text_surface = font.render(char, True, (255, 255, 255))
+        pygame.image.save(text_surface, os.path.join(output_dir, f"none-FONT_{char}.png"))
+
+    # 0~9までの文字画像をループで一気に生成
+    for char_code in range(ord('0'), ord('9') + 1):
+        char = chr(char_code)
+        # 文字を画像化（白色）
+        text_surface = font.render(char, True, (255, 255, 255))
+        pygame.image.save(text_surface, os.path.join(output_dir, f"none-FONT_{char}.png"))
+
+    # ハイフンの文字画像を生成
+    text_surface = font.render(chr(45), True, (255, 255, 255))
+    pygame.image.save(text_surface, os.path.join(output_dir, "none-FONT_-.png"))
+
+    # ドットの文字画像を生成
+    text_surface = font.render(".", True, (255, 255, 255))
+    pygame.image.save(text_surface, os.path.join(output_dir, "none-FONT_..png"))
+
 if __name__ == "__main__":
     # os.makedirs("data/assets/Pacman", exist_ok=True)
     # os.makedirs("data/assets/upper_256", exist_ok=True)
     # os.makedirs("data/assets/lower_128", exist_ok=True)
-    os.makedirs("data/assets/misaki_64", exist_ok=True)
+    os.makedirs("data/assets/nonefont_32", exist_ok=True)
 
     # create_pacman_images("data/assets/Pacman")
     # create_font_upper_images("data/assets/upper_256")
     # create_font_lower_images("data/assets/lower_128")
-    create_font_misaki_images("data/assets/misaki_64")
+    create_font_none_images("data/assets/nonefont_32")
     print("画像の自動生成が完了しました！")
