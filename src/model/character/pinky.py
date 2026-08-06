7ea2d4446b8c435dcd3d8420da092da2928b8c18
Naoaki Sato <nsato@c5r2s11.42tokyo.jp>
2026-08-06 17:24:39 +0900

    feat: 42マスの処理を削除。docstringを修正。ファイル冒頭が未記載。

--- src/model/character/pinky.py
+++ src/model/character/pinky.py
@@ -6,13 +6,13 @@ from src.model.base_model.character import Direction
 from src.model.map import Map
 
 
-# --- 「パックマンが今向いている方向の、4マス先の座標」を狙って移動
 class Pinky(Ghost):
     """GhostのPinkyを表すクラス。
-    追いかけるターゲットは「パックマンが今向いている方向の、4マス先の座標」。
+
+    Pinky、ピンキー、待ちぶせ、speedy、素早いキャラクター。
 
     Attributes:
-        images (dict[str, pygame.Surface]): キャラクターの画像を格納する辞書
+        images (dict[str, pygame.Surface]): キャラクターの画像を格納する辞書。
     """
     def __init__(self, x: int, y: int, px: int, py: int, speed: int, color: tuple[int, int, int], points: int) -> None:
         super().__init__(x, y, px, py, speed, color, points)
@@ -24,10 +24,10 @@ class Pinky(Ghost):
                 self.images[key] = pygame.image.load(f"assets/ghost/ghost_pinky_{key}.png")
 
     def level_up(self, game_state: GameState) -> None:
-        """クリア後のレベルアップ処理
+        """クリア後のレベルアップ処理。
 
         Args:
-            game_state (GameState): ゲームの状態を保持するGameStateオブジェクト
+            game_state (GameState): ゲームの状態を保持するGameStateオブジェクト。
         """
         assert game_state.map is not None
         map: Map = game_state.map
@@ -38,13 +38,15 @@ class Pinky(Ghost):
         self.direction = Direction.LEFT
 
     def _get_target(self, game_state: GameState) -> tuple[int, int]:
-        """ゴーストの移動目標座標を取得する
+        """ゴーストの移動目標座標を取得する。
+
+        パックマンが今向いている方向の、4マス先の座標を狙って移動する。
 
         Args:
-            game_state (GameState): ゲームの状態を保持するオブジェクト
+            game_state (GameState): ゲームの状態を保持するオブジェクト。
 
         Returns:
-            tuple[int, int]: ゴーストの移動目標座標
+            tuple[int, int]: ゴーストの移動目標座標。
         """
         assert game_state.map is not None
         map: Map = game_state.map
@@ -63,18 +65,5 @@ class Pinky(Ghost):
         # pinkyの目標座標は4マス先か、マップ外ならマップ内の端に設定
         target_x = max(0, min(map.x - 1, pacman.x + offset_x * 4))
         target_y = max(0, min(map.y - 1, pacman.y + offset_y * 4))
-
-        while map.is_reachable(target_x, target_y) is False:
-            print("Pinky入れた入れた入れた入れた入れた入れた入れた入れた")
-            if pacman.direction == Direction.LEFT or pacman.direction == Direction.RIGHT:
-                if target_x > pacman.x:
-                    target_x -= 1
-                elif target_x < pacman.x:
-                    target_x += 1
-            if pacman.direction == Direction.UP or pacman.direction == Direction.DOWN:
-                if target_y > pacman.y:
-                    target_y -= 1
-                elif target_y < pacman.y:
-                    target_y += 1
 
         return (target_x, target_y)
